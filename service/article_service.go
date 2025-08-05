1388284434d0fc7a43b38adfa2d5cfedb4ffc4cc
Pison Golda <pison.studyroom@gmail.com>
2025-08-05 13:12:18 +0700

    feat: add ArticleService interface with CreateArticle method

---
+++ service/article_service.go
@@ -0,0 +1,10 @@
+package service
+
+import (
+	"context"
+	"rest-blog-api/model/web"
+)
+
+type ArticleService interface {
+	CreateArticle(ctx context.Context, req web.ArticleCreateRequest) web.ArticleResponse
+}
