1d5cc286d149d107b693504e6e275273087d96f4
Pison Golda <pison.studyroom@gmail.com>
2025-08-05 14:31:29 +0700

    feat: implement CreateArticle in ArticleControllerImpl

---
+++ controller/article_controller_impl.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"net/http"
+	"rest-blog-api/helper"
+	"rest-blog-api/model/web"
+	"rest-blog-api/service"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type ArticleControllerImpl struct {
+	ArticleService service.ArticleService
+}
+
+func (controller *ArticleControllerImpl) CreateArticle(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	result := web.ArticleCreateRequest{}
+	helper.ReadFromRequestBody(r, result)
+
+	articleResponse := controller.ArticleService.CreateArticle(r.Context(), result)
+
+	webResponse := web.WebResponse{
+		Code:   201,
+		Status: "Created",
+		Data:   articleResponse,
+	}
+	helper.WriteToResponseBody(w, webResponse)
+}
