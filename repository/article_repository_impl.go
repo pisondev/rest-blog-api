2f1f2bc7c11a63cead0af74a2ed86ac69e12a88b
Pison Golda <pison.studyroom@gmail.com>
2025-08-05 14:36:37 +0700

    feat: add NewArticleRepository constructor

--- repository/article_repository_impl.go
+++ repository/article_repository_impl.go
@@ -10,6 +10,10 @@ import (
 type ArticleRepositoryImpl struct {
 }
 
+func NewArticleRepository() ArticleRepository {
+	return &ArticleRepositoryImpl{}
+}
+
 func (repository *ArticleRepositoryImpl) CreateArticle(ctx context.Context, tx *sql.Tx, article domain.Article) domain.Article {
 	SQL := "INSERT INTO articles(title, content) VALUES (?,?)"
 	result, err := tx.ExecContext(ctx, SQL, article.Title, article.Content)
