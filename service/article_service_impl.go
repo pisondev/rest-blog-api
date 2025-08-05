b65e208a10cf0cef60b5f5132414b3cc1b71104e
Pison Golda <pison.studyroom@gmail.com>
2025-08-05 13:35:45 +0700

    refactor: replace transaction handling with helper CommitOrRollback

--- service/article_service_impl.go
+++ service/article_service_impl.go
@@ -17,18 +17,7 @@ type ArticleServiceImpl struct {
 func (service *ArticleServiceImpl) CreateArticle(ctx context.Context, req web.ArticleCreateRequest) web.ArticleResponse {
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
-
-	defer func() {
-		r := recover()
-		if r != nil {
-			errRollback := tx.Rollback()
-			helper.PanicIfError(errRollback)
-			panic(r)
-		} else {
-			errCommit := tx.Commit()
-			helper.PanicIfError(errCommit)
-		}
-	}()
+	helper.CommitOrRollback(tx)
 
 	article := domain.Article{
 		Title:   req.Title,
