3c9947177b2389f90f063bc13fcc3fa61d74ef42
Pison Golda <pison.studyroom@gmail.com>
2025-08-06 12:02:52 +0700

    feat: add UpdateById in ArticleController interface

--- controller/article_controller.go
+++ controller/article_controller.go
@@ -10,4 +10,5 @@ type ArticleController interface {
 	CreateArticle(w http.ResponseWriter, r *http.Request, params httprouter.Params)
 	FindAllArticles(w http.ResponseWriter, r *http.Request, params httprouter.Params)
 	FindById(w http.ResponseWriter, r *http.Request, params httprouter.Params)
+	UpdateById(w http.ResponseWriter, r *http.Request, params httprouter.Params)
 }
