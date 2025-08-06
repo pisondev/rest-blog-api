d8067ee8fb97a9b9320e7cb563e2e4219705aaf2
Pison Golda <pison.studyroom@gmail.com>
2025-08-06 14:02:37 +0700

    fix: rename used_at to updated_at in JSON tag

--- model/web/article_response.go
+++ model/web/article_response.go
@@ -7,5 +7,5 @@ type ArticleResponse struct {
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"used_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
