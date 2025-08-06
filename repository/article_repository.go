d8e627216fcc3214e8218d71a60863c82ee60815
Pison Golda <pison.studyroom@gmail.com>
2025-08-06 11:00:54 +0700

    feat: add FindById in ArticleRepository interface

--- repository/article_repository.go
+++ repository/article_repository.go
@@ -9,4 +9,5 @@ import (
 type ArticleRepository interface {
 	CreateArticle(ctx context.Context, tx *sql.Tx, article domain.Article) domain.Article
 	FindAllArticles(ctx context.Context, tx *sql.Tx) []domain.Article
+	FindById(ctx context.Context, tx *sql.Tx, articleId int) (domain.Article, error)
 }
