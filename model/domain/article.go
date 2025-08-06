f1116375c9ea42743f7358a445d529b7d8ca8aac
Pison Golda <pison.studyroom@gmail.com>
2025-08-06 13:49:47 +0700

    feat: add CreatedAt & UpdatedAt in the Article domain model

--- model/domain/article.go
+++ model/domain/article.go
@@ -1,7 +1,11 @@
 package domain
 
+import "time"
+
 type Article struct {
-	Id      int
-	Title   string
-	Content string
+	Id        int
+	Title     string
+	Content   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
