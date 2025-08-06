285cff1eeffb9a99cb043af6f3babdb7f334f534
Pison Golda <pison.studyroom@gmail.com>
2025-08-06 13:46:57 +0700

    feat: add CreatedAt & UpdatedAt in the ArticleResponse

--- model/web/article_response.go
+++ model/web/article_response.go
@@ -1,7 +1,11 @@
 package web
 
+import "time"
+
 type ArticleResponse struct {
-	Id      int    `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	Id        int       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"used_at"`
 }
