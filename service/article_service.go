532ed1800f213df807ad31e7563aed13ba1d13c8
Pison Golda <pison.studyroom@gmail.com>
2025-08-06 11:10:33 +0700

    feat: add FindById in ArticleService interface

--- service/article_service.go
+++ service/article_service.go
@@ -8,4 +8,5 @@ import (
 type ArticleService interface {
 	CreateArticle(ctx context.Context, req web.ArticleCreateRequest) web.ArticleResponse
 	FindAllArticles(ctx context.Context) []web.ArticleResponse
+	FindById(ctx context.Context, categoryId int) web.ArticleResponse
 }
