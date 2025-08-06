0f308969ac4e14235c27bdc516679e608544ee43
Pison Golda <pison.studyroom@gmail.com>
2025-08-06 11:58:23 +0700

    feat: add UpdateById in ArticleService interface

--- service/article_service.go
+++ service/article_service.go
@@ -9,4 +9,5 @@ type ArticleService interface {
 	CreateArticle(ctx context.Context, req web.ArticleCreateRequest) web.ArticleResponse
 	FindAllArticles(ctx context.Context) []web.ArticleResponse
 	FindById(ctx context.Context, categoryId int) web.ArticleResponse
+	UpdateById(ctx context.Context, req web.ArticleUpdateRequest) web.ArticleResponse
 }
